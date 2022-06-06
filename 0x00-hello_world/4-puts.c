eb41e45eb391b9c9b5cbdaf3d221f721a1f74351
Daniel Igwe <daniel.igwe000@gmail.com>
2022-06-05 17:04:33 -0700

    C-Hello World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
+/**
+ * This function writes to stdin using put
+ */
 #include <stdio.h>
 
 int main(void)
 {
-	char string [] = "Programming is like building a multilingual puzzle";
+	char string[] = "Programming is like building a multilingual puzzle";
 	puts(string);
 	return(0);
 }
