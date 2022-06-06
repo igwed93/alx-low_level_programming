b5fdb604a539721739f1a0c5bde4872f0c135f1a
Daniel Igwe <daniel.igwe000@gmail.com>
2022-06-05 17:31:46 -0700

    C-Hello World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,10 +5,8 @@
  * 
  * Return: Always 0 (Success)
  */
-
-int main (void)
+int main(void)
 {
-	char *string = "Programming is like building a multilingual puzzle";
-	puts(string);
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
