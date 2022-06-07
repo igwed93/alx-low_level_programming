bc16478d6f37d7bdd1db19200d2fe37dc7aedb21
Daniel Igwe <daniel.igwe000@gmail.com>
2022-06-07 14:11:55 -0700

    For Statement

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ * This programs return alphabets in lower case
+ */
+int main(void)
+{
+	char low;
+
+	for (low = 'a'; low <= 'z'; low++)
+		putchar(low);
+		printf("\n");
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,3 +1,4 @@
 Introduction to conditonal statements "if, else, while"
 0-positive_or_negative.c demonstrates the use of the if statements
 1-last_digit displays the last digit of any random number given
+2-print_alphabet.c prints alphabets in lower case
