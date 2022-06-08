abddca26aac50084c67f7dd1153cfba200f78979
Daniel Igwe <daniel.igwe000@gmail.com>
2022-06-07 19:08:49 -0700

    variables if else while

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ * This program prints all possible combinations of two digits
+ *
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 90; n++)
+	{
+		putchar((n / 10) + '0');
+		putchar((n % 10) + '0');
+
+		if (n != 89)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -9,3 +9,4 @@ Introduction to conditonal statements "if, else, while"
 7-print_tebahpla.c prints the lower case alphabets in reverse order
 8-print_base16.c prints all the numbers of base 16 in lowercase
 9-print_comb.c prints all possible combinations of single-digit numbers
+100-print_comb3.c prints all possible different combinations of two digits
