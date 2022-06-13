eb3e7b09125c3de6c482eaf1315dc1195a1ebbae
Daniel Igwe <daniel.igwe000@gmail.com>
2022-06-12 20:22:54 -0700

    functions nested loops

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -4,12 +4,12 @@
  * print_last_digit - prints the last digit of a number
  * @n: the integer whose last digit is printed
  *
- * Return: always 0 (Success)
+ * Return: returns the last digit of the number
  */
 int print_last_digit(int n)
 {
 	int L = n % 10;
 
 	_putchar(L);
-	return (0);
+	return (L);
 }
