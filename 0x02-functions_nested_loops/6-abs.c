f3a0ba083bfc4e4df8db5919f4f0f3faee9aba52
Daniel Igwe <daniel.igwe000@gmail.com>
2022-06-12 19:59:49 -0700

    functions nested loops

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,17 +3,17 @@
 /**
  * _abs - computes the absolute value of any given integer
  *@n: the integer that is computed
- * Return: always 0 (Success)
+ * Return: the computed absolute value is returned.
  */
 int _abs(int n)
 {
 	if (n < 0)
 	{
 		n *= -1;
-		_putchar(n);
+		return (n);
 	}
 	else if (n >= 0)
 	{
-		putchar(n);
+		return (n);
 	}
 }
