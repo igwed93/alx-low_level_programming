7a63502d0c734a1dbcd00d7a86b84162d2b7d430
Daniel Igwe <daniel.igwe000@gmail.com>
2022-06-14 10:08:49 -0700

    pointers arrays and strings

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_array - prints n elements of an array of integers
+ * @a: array parameter
+ * @n: integer parameter
+ *
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d\n", a[i]);
+	}
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -6,3 +6,4 @@
 5-rev_string reverses a string
 6-puts2.c prints every other character of a string, starting with the first character, followed by a newline
 7-puts_half.c prints half of a string
+prints n elements of an array of integers
