89265eebf541345f60b76725230b6272776e39b7
Daniel Igwe <daniel.igwe000@gmail.com>
2022-06-23 13:02:45 -0700

    fixed output errors in 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,13 +8,18 @@
 void _puts_recursion(char *s)
 {
 	int i = 0;
+	
+	if (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		s++;
+		_puts_recursion(s);
+	}
 
-	_putchar(s[i]);
 	if (s[i] == '\0')
 	{
 		_putchar('\n');
 		return;
 	}
-	s++;
-	_puts_recursion(s);
+
 }
