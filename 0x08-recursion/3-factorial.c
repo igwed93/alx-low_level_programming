dab291875e2230d02ed22f3a463ec7364ee6ad2e
Daniel Igwe <daniel.igwe000@gmail.com>
2022-06-24 06:21:38 -0700

    Included the file 3-factorial.c

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * factorial - function that returns the factorial of an int.
+ * @n: any given positive integer
+ *
+ * return: the factorial of int n.
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+
+	return (n * factorial(n - 1));
+}
--- 0x08-recursion/README.md
+++ 0x08-recursion/README.md
@@ -2,3 +2,4 @@ This is the readme file for 0x08-recursion
 0-puts_recursion.c is a function that prints a string
 1-print_rev_recursion.c is a function that reverses a string
 2-strlen_recursion.c is a function that returns the length of a string
+3-factorial.c is a function that returns the factorial of a positive integer
