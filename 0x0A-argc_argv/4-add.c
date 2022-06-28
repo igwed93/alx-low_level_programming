e5cf1a8b721369df509cfd7476a7e44390aa09d1
Daniel Igwe <daniel.igwe000@gmail.com>
2022-06-28 12:40:16 -0700

    Added some codes to 4-add.c to fix some bugs

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int numChecker(char *s);
+
+/**
+ * numChecker - function that checks if a number is a n integer
+ * @s: pointer to a character
+ *
+ * Return: 0 if successful and 1 otherwise.
+ */
+int numChecker(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
+	}
+	return (1);
+}
+
 /**
  * main - program that returns the some of positive integers
  * @argc: argument count
@@ -19,7 +39,12 @@ int main(int argc, char *argv[])
 	}
 	for (i = 1; i < argc; i++)
 	{
-		if (!(atoi(argv[i])))
+		if (numChecker(argv[i]) == 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else if (atoi(argv[i]) <= 0)
 		{
 			printf("Error\n");
 			return (1);
