76e162e0c33e5a881b02c6c6db11a2b621cbb1db
Daniel Igwe <daniel.igwe000@gmail.com>
2022-07-01 07:28:35 -0700

    Included 2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _calloc - a function that allocates memory for an array
+ * @nmemb: number of elements in the array
+ * @size: size of each element of the array
+ *
+ * Return: a pointer to the allocated memory on success
+ * otherwise if malloc fails or nmemb or size is 0 it
+ * returns NULL.
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *ptr; /*Declare a pointer to be used*/
+
+	/*reserve memory spaces in heap*/
+	ptr = calloc(nmemb, size);
+
+	/*check that calloc doesnt fail*/
+	if (ptr == NULL)
+		return (NULL);
+
+	/*if nmemb or size is 0 return NULL*/
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	return (ptr);
+}
--- 0x0C-more_malloc_free/README.md
+++ 0x0C-more_malloc_free/README.md
@@ -1,2 +1,3 @@
 0x0C C-malloc, free hosts functions/programs that exemplify the usage of malloc, calloc, realloc, and free
 1-string_nconcat is a function that concatenates two strings
+2-calloc.c is a function that allocates memory for an array using malloc
