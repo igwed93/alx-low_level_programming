4fd8249f6bf7681b19e462c981153ad768461d97
Daniel Igwe <daniel.igwe000@gmail.com>
2022-07-07 15:39:37 -0700

    Included 5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - function that frees dogs
+ * @d: variable to data type
+ *
+ * Return: nothing.
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+	return;
+
+}
