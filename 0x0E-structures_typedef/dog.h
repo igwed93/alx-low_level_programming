81fa8b4e3bd56fda6ba01b0aeeb4b24f98bc001b
Daniel Igwe <daniel.igwe000@gmail.com>
2022-07-07 11:54:08 -0700

    Handled double inclusion

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,22 @@
+#ifndef DOG_FILE
+#define DOG_FILE
+
+/**
+ * struct dog - the biodata of a dog
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ *
+ * Description: this data type stores a comprehensive info
+ * about dogs. e.g their names, age, and owner's name.
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+typedef struct dog dog_t;
+
+#endif
