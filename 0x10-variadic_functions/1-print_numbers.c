8978d4aec3648bce27ffa160320c39f1da115327
Daniel Igwe <daniel.igwe000@gmail.com>
2022-07-09 15:41:31 -0700

    Modified the code to handle some exceptions

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -6,6 +6,7 @@
  * print_numbers - function that print numbers followed by a new line
  * @separator: string to be printed between numbers
  * @n: number of arguements to the function
+ * @...: variadic parameters
  *
  * Return: nothing.
  */
@@ -18,14 +19,12 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
+		printf("%d", (va_arg(num, int)));
 		if (separator && n > 0 && i < n - 1)
 		{
-			printf("%d%s", (va_arg(num, int)), separator);
-		}
-		else if (separator && (i >= n - 1 || n == 1))
-		{
-			printf("%d\n", (va_arg(num, int)));
+			printf("%s", separator);
 		}
 	}
+	printf("\n");
 	va_end(num);
 }
