927e5263987c84a6b57af78c39144c188366af76
Daniel Igwe <daniel.igwe000@gmail.com>
2022-07-09 15:22:23 -0700

    Fixed bugs relating to the NULL value of the separator

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -18,10 +18,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		if (separator == NULL)
-		{
-			printf("%d", (va_arg(num, int)));
-		}
 		if (separator && i < n - 1)
 		{
 			printf("%d%s", (va_arg(num, int)), separator);
