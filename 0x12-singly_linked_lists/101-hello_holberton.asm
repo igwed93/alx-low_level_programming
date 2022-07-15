52d52177b39a8f9507e7fd4fca6f063e07de31db
Daniel Igwe <daniel.igwe000@gmail.com>
2022-07-14 18:38:10 -0700

    Fixed errors in 101-hello_holberton.c

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,20 +1,17 @@
-; ----------------------------------------------------------------------------------------
-;
-; Writes "Hello, World" to the console using only system calls. Runs on 64-bit Linux only.
-;
-; ----------------------------------------------------------------------------------------
+section .data
+	fmt db	"%s", 10, 0
+	msg db	"Hello, Holberton", 0
 
-          global    _start
+	section .text
+	extern printf
+	global main
 
-          section   .text
-_start:   mov       rax, 1                  ; system call for write
-          mov       rdi, 1                  ; file handle 1 is stdout
-          mov       rsi, message            ; address of string to output
-          mov       rdx, 13                 ; number of bytes
-          syscall                           ; invoke operating system to do the write
-          mov       rax, 60                 ; system call for exit
-          xor       rdi, rdi                ; exit code 0
-          syscall                           ; invoke operating system to exit
-
-          section   .data
-message:  db        "Hello, Holberton", 10      ; note the newline at the end
+main:
+	push	rbp
+	mov	rsi, msg
+	mov	rdi, fmt
+	mov	rax, 0
+	call 	printf
+	pop	rbp
+	mov	rax, 0
+	ret
