5edeab50a93f8483c1aff850414e449345143c25
Daniel Igwe <daniel.igwe000@gmail.com>
2022-07-14 17:37:54 -0700

    Added: 100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * funny_func - function that executes before the main program
+ *
+ * Return: void
+ */
+void funny_func(void)__attribute__((constructor))
+
+void funny_func(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
