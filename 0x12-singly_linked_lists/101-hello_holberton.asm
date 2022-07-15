685ddd2c096fa08892960904e8fef8c1f593ef3e
Daniel Igwe <daniel.igwe000@gmail.com>
2022-07-14 18:03:46 -0700

    Included: 101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,16 +1,16 @@
-section .data
-    msg db "Hello world!",10
+	global _start
 
-section .text
-    global _start
+	section .text
 
-_start:
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, msg
-    mov rdx, 13
-    syscall
-    
-    mov rax, 60
-    mov rdi, 0
-    syscall
+_start:	mov rax, 1        ; write(
+	mov rdi, 1        ;   STDOUT_FILENO,
+	mov rsi, msg      ;   "Hello, world!\n",
+	mov rdx, msglen   ;   sizeof("Hello, world!\n")
+	syscall           ; );
+	mov rax, 60       ; exit(
+	mov rdi, rdi        ;   EXIT_SUCCESS
+	syscall           ; );
+
+	section .rodata
+msg:	db "Hello, world!", 10
+msglen:	equ $ - msg
