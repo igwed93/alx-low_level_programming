8cd99569adfa2578fdfbec2a86a6deff1d162c6f
Daniel Igwe <daniel.igwe000@gmail.com>
2022-07-18 13:14:20 -0700

    Removed type casting to fix some bugs

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -11,7 +11,7 @@
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	/* allocate node */
-	listint_t *new = (listint_t *)malloc(sizeof(listint_t));
+	listint_t *new = malloc(sizeof(listint_t));
 
 	/* verify that malloc did not fail */
 	if (new == NULL)
