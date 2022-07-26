28216717c005d0a26e6deed50ecec9e58b3c3a39
Daniel Igwe <daniel.igwe000@gmail.com>
2022-07-26 14:08:22 -0700

    Fixing bugs

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,7 +8,7 @@ void close_file(int fd)
 {
 	if (close(fd) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't clode fd %d\n", fd);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
 	}
 }
