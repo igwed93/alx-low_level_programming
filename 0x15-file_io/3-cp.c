5fb86847f31f67f9e35879f5a50444f57249a48f
Daniel Igwe <daniel.igwe000@gmail.com>
2022-07-25 20:32:05 -0700

    Wrote and Included thhe code for: 3-cp.c

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,114 @@
+#include "main.h"
+
+/**
+ * close_file - close file and check if was successful
+ * @fd: file descriptor
+ */
+void close_file(int fd)
+{
+	if (close(fd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't clode fd %d\n", fd);
+		exit(100);
+	}
+}
+
+/**
+ * err_handler - returns requisite exit status
+ * @fd: file descriptor
+ * @fname: file descriptor
+ * @type: type of sys call
+ * @buffer: buffer to be free if exit status
+ * Return: 0 on success, exit status on error
+ */
+int err_handler(int fd, char *fname, int type, char *buffer)
+{
+	int status = 0;
+	(void)fd;
+
+	switch (type)
+	{
+	case READ_ERROR:
+		dprintf(STDERR_FILENO,
+			"Error: Can't read from file %s\n", fname);
+		if (buffer != NULL)
+			free(buffer);
+		status = 98;
+		break;
+	case WRITE_ERROR:
+		dprintf(STDERR_FILENO,
+			"Error: Can't write to %s\n", fname);
+		if (buffer != NULL)
+			free(buffer);
+		status = 99;
+		break;
+	default:
+		break;
+	}
+		return (status);
+}
+
+
+/**
+ * make_buffer - allocates a buffer of BUFFERSIZE
+ * @fName: file destination to print error
+ * Return: pointer to new allocated buffer
+ */
+char *make_buffer(char *fName)
+{
+	char *buffer;
+
+	buffer = malloc(sizeof(char) * BUFFERSIZE);
+	if (buffer == NULL)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", fName);
+		exit(99);
+	}
+	return (buffer);
+}
+
+/**
+ * main - copies contents of 1 file to another
+ * @argc: number of parameters
+ * @argv: array of parameter
+ *
+ * Return: Always 0 (success).
+ */
+int main(int argc, char *argv[])
+{
+	int fd_src, fd_dest, output;
+	char *file_t; /*buffer*/
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	fd_src = open(argv[1], O_RDONLY);
+	if (fd_src < -1)
+		exit(err_handler(fd_src, argv[1], READ_ERROR, NULL));
+
+	file_t = make_buffer(argv[2]);
+
+	umask(0);
+	fd_dest = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
+	if (fd_dest < -1)
+		exit(err_handler(fd_dest, argv[2], WRITE_ERROR, file_t));
+
+	do {
+		output = read(fd_src, file_t, BUFFERSIZE);
+		if (output == -1)
+			exit(err_handler(output, argv[1], READ_ERROR, file_t));
+		output = write(fd_dest, file_t, output);
+		if (output == -1)
+			exit(err_handler(output, argv[2], WRITE_ERROR, file_t));
+		fd_dest = open(argv[2], O_RDWR | O_APPEND);
+		if (fd_dest == -1)
+			exit(err_handler(fd_dest, argv[2], WRITE_ERROR, file_t));
+	} while (output > 0);
+	close_file(fd_src);
+	close_file(fd_dest);
+	free(file_t);
+	return (0);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/* Libraries */
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -8,9 +9,19 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
+/* prototypes of used functions */
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+char *create_buffer(char *fileName);
+int error_handler(int fd, char *fname, int type, char *buffer);
+void close_file(int fd);
+
+/* MACROS */
+#define BUFFERSIZE 1024
+#define READ_ERROR 1
+#define WRITE_ERROR 2
+
 
 #endif
