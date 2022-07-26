284535714761ae63bf60594d1f13350395d35163
Daniel Igwe <daniel.igwe000@gmail.com>
2022-07-25 18:27:53 -0700

    Wrote and included the code for: 2-append_text_to_file.c

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * append_text_to_file -  a function that creates a file
+ * @filename: name of the file to create
+ * @text_content: string to write to the created file
+ *
+ * Return: 1 on success and -1 if an error occurs
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, count, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	/* open file and get file descriptor */
+	fd = open(filename, 2 | O_APPEND, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		while (text_content[len])
+			len++;
+	}
+
+	/* write into file description */
+	count = write(fd, text_content, len);
+
+	if (count == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+	close(fd);
+
+	return (1);
+}
--- 0x15-file_io/README.md
+++ 0x15-file_io/README.md
@@ -1,3 +1,4 @@
 0x15. C - File I/O exemplifies system calls and function that can be used to manipulate file inputs/outputs
 0-read_textfile.c is a function that reads a text file and prints it to the POSIX standard output
 1-create_file.c is a function that creates a file
+2-append_text_to_file.c is a function that appends text at the end of a file 
