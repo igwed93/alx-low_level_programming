b8e891ec74a124db0b644191be3c77b96a8c39f4
Daniel Igwe <daniel.igwe000@gmail.com>
2022-07-26 22:17:22 -0700

    Fixing bugs

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -79,6 +79,7 @@ int main(int argc, char *argv[])
 	int fd_src, fd_dest, result;
 	char *buffer; /*buffer*/
 
+	(void)argc;
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
