a3c62d188134b53dadd6ef6dead44d81c4e229d3
Daniel Igwe <daniel.igwe000@gmail.com>
2022-07-29 18:39:19 -0700

    Wrote and Included the code for: 100-elf_header.c

--- 0x15-file_io/README.md
+++ 0x15-file_io/README.md
@@ -3,3 +3,4 @@
 1-create_file.c is a function that creates a file
 2-append_text_to_file.c is a function that appends text at the end of a file 
 3-cp.c is a program that copies the content of a file to another file
+100-elf_header.c is a program that displays the information contained in the ELF header at the start of an ELF file
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 
 /* Libraries */
+#include <elf.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -17,6 +18,18 @@ int append_text_to_file(const char *filename, char *text_content);
 char *create_buffer(char *fileName);
 int error_handler(int fd, char *fname, int type, char *buffer);
 void close_file(int fd);
+
+/* prototypes of functions for advanced task */
+void check_elf(unsigned char *e_ident);
+void print_magic(unsigned char *e_ident);
+void print_class(unsigned char *e_ident);
+void print_data(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_abi(unsigned char *e_ident);
+void print_osabi(unsigned char *e_ident);
+void print_type(unsigned int e_type, unsigned char *e_ident);
+void print_entry(unsigned long int e_entry, unsigned char *e_ident);
+void close_elf(int elf);
 
 /* MACROS */
 #define BUFFERSIZE 2900
