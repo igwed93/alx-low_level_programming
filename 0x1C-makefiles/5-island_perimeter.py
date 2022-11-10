c1ae99963f19e8d26cf390ba35f94633fdf907f3
Daniel Igwe <daniel.igwe000@gmail.com>
2022-11-10 02:10:15 +0100

    Island Perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -13,18 +13,23 @@ def island_perimeter(grid):
             0 represents a water zone & 1 a land zone.
             One cell is a square with side length 1.
             Grid cells are connected horizontally/vertically.
-            Grid is rectangular, width and height <= 100
+            Grid is rectangular, width and height don't exceed 100
+            Grid is completely surrounded by water, and there is one island
     """
 
-    vertical = 0  # number of vertical cells
-    horizontal = 0  # number of horizontal cells
+    width = len(grid[0])
+    height  = len(grid)
+    grid_edges = 0
+    grid_size = 0
 
-    for v in range(len(grid)):
-        if 1 in grid[v]:
-            vertical += 1
-        if (grid[v]).count(1) > 1:
-            horizontal += (grid[v]).count(1)
-        continue
-    print(vertical)
-    print(horizontal)
-    return (2*(vertical + horizontal))
+    for v in range(height):
+        for h in range(width):
+            if grid[v][h] == 1:
+                grid_size += 1
+                if (h > 0 and grid[v][h - 1] == 1):
+                    grid_edges += 1
+                if (v > 0 and grid[v - 1][h] == 1):
+                    grid_edges += 1
+    print(grid_size)
+    print(grid_edges)
+    return (grid_size * 4 - grid_edges * 2)
